f55c0c6cf4c604c31db2281287ec621c8b4b3b28
Lisa Clark <lisa.clark@rackspace.com>
2013-04-29 13:27:30 -0500

    D-12359 - fixing multimatch error msg

--- project-set/extensions/api-validator/src/test/java/org/openrepose/components/apivalidator/filter/ApiValidatorHandlerTest.java
+++ project-set/extensions/api-validator/src/test/java/org/openrepose/components/apivalidator/filter/ApiValidatorHandlerTest.java
@@ -116,5 +116,23 @@ public class ApiValidatorHandlerTest {
             verify(blowupValidator).validate(request, response, chain);
             assertEquals(HttpStatusCode.BAD_GATEWAY, director.getResponseStatus());
         }
+
+        @Test
+        public void shouldAddDefaultValidatorAsLeastPriorityWhenMultiMatch() {
+            List<HeaderValue> roles = new ArrayList<HeaderValue>();
+            roles.add(new HeaderValueImpl("role1"));
+
+            List<ValidatorInfo> validators = new ArrayList<ValidatorInfo>();
+            validators.add(role1ValidatorInfo);
+            validators.add(role2ValidatorInfo);
+
+            instance = new ApiValidatorHandler(defaultValidatorInfo, validators, true);
+
+            List<ValidatorInfo> validatorsForRole = instance.getValidatorsForRole(roles);
+
+            assertEquals(validatorsForRole.get(0), defaultValidatorInfo);
+            assertEquals(validatorsForRole.get(1), role1ValidatorInfo);
+        }
+
     }
 }
