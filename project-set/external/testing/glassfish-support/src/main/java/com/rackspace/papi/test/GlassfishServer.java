57b8919f5f2d72f605496f92bc166a21bc8515e3
Lisa Clark <lisa.clark@rackspace.com>
2013-08-22 17:07:33 -0500

    mucking with glassfish launcher

--- project-set/external/testing/glassfish-support/src/main/java/com/rackspace/papi/test/GlassfishServer.java
+++ project-set/external/testing/glassfish-support/src/main/java/com/rackspace/papi/test/GlassfishServer.java
@@ -15,43 +15,9 @@ public class GlassfishServer {
 
     public static void main(String[] args) {
 
-        final CmdLineParser cmdLineParser = new CmdLineParser(commandLineArgs);
-
-        try {
-            cmdLineParser.parseArgument(args);
-        } catch (CmdLineException e) {
-
-            displayUsage(cmdLineParser, e);
-            return;
-        }
-
-        if (!validPorts(commandLineArgs)) {
-            return;
-        }
-
-        try{
-            validateConfigDirectory(commandLineArgs);
-        }catch(IOException e){
-            System.err.println(e.getMessage());
-            cmdLineParser.printUsage(System.err);
-            return;
-        }
-
-        final PowerApiValveServerControl serverControl = new PowerApiValveServerControl(commandLineArgs);
-
-        if (commandLineArgs.getAction().equalsIgnoreCase(CommandLineArguments.ACTION_START)) {
-            serverControl.startPowerApiValve();
-        }
-        if (commandLineArgs.getAction().equalsIgnoreCase(CommandLineArguments.ACTION_STOP)) {
-            serverControl.stopPowerApiValve();
-        }
-
         EmbeddedFileSystem.Builder builder = new EmbeddedFileSystem.Builder();
 
         builder.autoDelete(true);
-
-
-        server = builder.
     }
 
 }
